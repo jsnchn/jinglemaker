faae713714c4572b3d18a8d27473a05709f002f9
Jason Chen <chen.visual@gmail.com>
2014-06-11 00:26:56 -0400

    UI updates

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -4,11 +4,16 @@ angular.module('jinglemakerApp')
 	.controller('MainCtrl', function ($scope) {
     $scope.bpm = '120';
     $scope.increment = '1/1';
+    $scope.octaves = ['2','3','4','5','6','7','8','9'];
+    $scope.keys = ['c', 'c#', 'd', 'd#', 'e', 'f', 'f#', 'g', 'g#', 'a', 'a#', 'b'];
 
-    var oneRow  = '';
-    for (var i = 0 ; i < 85 ; i++){
-    	oneRow += '<div class="noteCol"></div>';
-    }
-    $('#track').html(oneRow);
+
+    // var oneRow  = '';
+    // for (var i = 0 ; i < 145 ; i++){
+    // 	oneRow += '<div class="noteCol"></div>';
+    // }
+    // $('#track').html(oneRow);
+
+
 
   });
